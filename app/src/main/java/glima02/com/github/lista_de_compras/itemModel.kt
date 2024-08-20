3cd839304dd666091b89c1bd581b5fa2265b8bc9
glima02 <guilherme.l.oliveira2011@gmail.com>
2024-08-19 22:23:23 -0300

    fim aula 19/08

---
+++ app/src/main/java/glima02/com/github/lista_de_compras/itemModel.kt
@@ -0,0 +1,3 @@
+package glima02.com.github.lista_de_compras
+
+data class ItemModel(val name: String)
---
+++ app/src/main/java/glima02/com/github/lista_de_compras/itemsAdapter.kt
@@ -0,0 +1,24 @@
+package carreiras.com.github.kotlin_android_lista_de_compras
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemsAdapter : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
+
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
